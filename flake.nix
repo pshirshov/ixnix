e2c23f520f9b3b42f92c659d2d832582d9cfce12
Pavel Shirshov <pshirshov@eml.cc>
2025-11-26 21:29:55 +0000

    wip

---
+++ flake.nix
@@ -0,0 +1,129 @@
+{
+  description = "Nix flake for the ix package manager";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = inputs@{ self, nixpkgs, flake-utils }:
+    let
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+      ];
+
+      ixRev = "1ac8f3629c1187104af166792dbe0420489f56b3";
+      ixSha256 = "1jlmi34vvkvsvga5z5bc9m688n9a5zcamvbgywixaqy822f69x13";
+      ixVersion = "unstable-2025-11-25";
+    in
+      flake-utils.lib.eachSystem systems (system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+          python = pkgs.python3;
+
+          runtimeTools = with pkgs; [
+            bash
+            coreutils
+            findutils
+            gnused
+            gnugrep
+            gawk
+            gnutar
+            gzip
+            bzip2
+            xz
+            zstd
+            unzip
+            curl
+            wget
+            git
+            rsync
+            patch
+            diffutils
+            openssl
+          ];
+
+          buildTools = with pkgs; [
+            clang
+            lld
+            llvm
+            gnumake
+            cmake
+            ninja
+            pkg-config
+          ];
+
+          ixSource = pkgs.fetchFromGitHub {
+            owner = "stal-ix";
+            repo = "ix";
+            rev = ixRev;
+            sha256 = ixSha256;
+          };
+
+          ix = pkgs.stdenv.mkDerivation {
+            pname = "ix";
+            version = ixVersion;
+            src = ixSource;
+            nativeBuildInputs = [ pkgs.makeWrapper ];
+            dontBuild = true;
+            strictDeps = true;
+
+            installPhase = ''
+              runHook preInstall
+
+              mkdir -p $out/libexec/ix
+              cp -r . $out/libexec/ix
+              chmod +x $out/libexec/ix/ix
+
+              mkdir -p $out/bin
+              makeWrapper ${python}/bin/python3 $out/bin/ix \
+                --add-flags "$out/libexec/ix/ix" \
+                --prefix PATH : ${pkgs.lib.makeBinPath runtimeTools}
+
+              install -Dm644 ix.1 $out/share/man/man1/ix.1
+
+              runHook postInstall
+            '';
+
+            doInstallCheck = true;
+            installCheckPhase = ''
+              PYTHONPATH="$out/libexec/ix:${python}/${python.sitePackages}" \
+                ${python}/bin/python - <<'PY'
+import importlib
+import sys
+
+sys.path.insert(0, "$out/libexec/ix")
+importlib.import_module("core.main")
+importlib.import_module("core.entry")
+PY
+            '';
+
+            meta = {
+              description = "Statically focused package manager";
+              homepage = "https://github.com/stal-ix/ix";
+              license = pkgs.lib.licenses.mit;
+              mainProgram = "ix";
+              platforms = systems;
+            };
+          };
+        in {
+          packages = {
+            inherit ix;
+            default = ix;
+          };
+
+          apps.default = flake-utils.lib.mkApp { drv = ix; };
+
+          devShells.default = pkgs.mkShell {
+            packages = runtimeTools
+              ++ buildTools
+              ++ [
+                python
+                pkgs.nix
+                ix
+              ];
+          };
+        });
+}
